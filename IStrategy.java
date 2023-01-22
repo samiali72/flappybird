a8a682a3abd2b24c76eb7c6e67b5587194b3f04e
samiali72 <123313300+samiali72@users.noreply.github.com>
2023-01-23 01:11:00 +0500

    Controller

---
+++ IStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flappybird.controller;
+
+import com.flappybird.model.Bird;
+import java.awt.event.KeyEvent;
+
+
+public interface IStrategy {
+    
+    public void controller(Bird bird, KeyEvent kevent);
+    public void controllerReleased(Bird bird, KeyEvent kevent);
+}
