e370cdde39a5a5b32dbf2214e337730d41321c6e
samiali72 <123313300+samiali72@users.noreply.github.com>
2023-01-23 01:12:35 +0500

    Proxy

---
+++ RealImage.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flappybird.model.proxy;
+
+import javax.swing.ImageIcon;
+
+
+public class RealImage implements IImage {
+
+    private final String src;
+    private ImageIcon imageIcon;
+    
+    public RealImage(String src) {
+        this.src = src;
+    }
+    @Override
+    public ImageIcon loadImage() {
+        if(imageIcon == null) {
+            this.imageIcon = new ImageIcon(getClass().getResource(src));
+        }
+        return imageIcon;
+    }
+    
+}
